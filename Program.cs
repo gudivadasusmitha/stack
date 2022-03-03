e70e218a5b2db806d529f0717b2e5a18a19f1ff1
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-03 12:25:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            stack.Push(20);
+            stack.Push(23);
+            stack.Push(43);
+            stack.Push(44);
+            stack.Push(89);
+            Console.Write("total number of elements in the stack are: ");
+            Console.WriteLine(stack.Count);
+            Console.Write("total number of elements in the stack are:" + stack.Peek());
+            Console.WriteLine("the element on the top of stack is:" + stack.Peek());
+            Console.Write("total number of elements in the stack are: ");
+            Console.WriteLine(stack.Count);
+            Console.ReadLine();
+        }
+    }
+}
